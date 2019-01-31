bc22a1cfcf04a9296f4e04ab544d2254dfdc8ff1
Parth Nagori <nagori.parth@gmail.com>
2019-01-31 01:55:03 -0500

    top 5 usage variables added to controller top 5 usage statistics displayed in dashboard view

--- src/app/dashboard/dashboard.js
+++ src/app/dashboard/dashboard.js
@@ -8,14 +8,14 @@ angular.module('dashboard', ['resources.devices', 'filters.formatting'])
       devices: ['Devices', function (Devices) {
         return Devices.all();
       }],
-      highCPU_devices: ['Devices', function (Devices) {
-        return Devices.sortByKey('cpuPct'); 
-      }]
     }
   });
 }])
 
-.controller('DashboardCtrl', ['$scope', '$location', 'devices', 'highCPU_devices', function ($scope, $location, devices, highCPU_devices) {
+.controller('DashboardCtrl', ['$scope', '$filter', '$location', 'devices', function ($scope, $filter, $location, devices) {
   $scope.devices = devices;
-  $scope.highCPU_devices = highCPU_devices;
+  $scope.highCPU_devices = $filter('orderBy')($scope.devices, '-cpuPct').slice(0,5);
+  $scope.highMEM_devices = $filter('orderBy')($scope.devices, '-memBytes').slice(0,5);
+  $scope.highRX_devices = $filter('orderBy')($scope.devices, '-networkRxBytes').slice(0,5);
+  $scope.highTX_devices = $filter('orderBy')($scope.devices, '-networkTxBytes').slice(0,5);
 }]);
